dee41fabfbc017fa5ceeba64426eb2fac128e0bd
Alex <ave.lutchak@gmail.com>
2018-07-18 00:09:36 +0300

    mod src

--- src/Rubic'sCube/Rubic'sCube/Game.cpp
+++ src/Rubic'sCube/Rubic'sCube/Game.cpp
@@ -14,37 +14,97 @@ Game::Game()
 
 void Game::fGameLoop()
 {
-	RubicsCube Blue_side('B');
-	RubicsCube Green_side('G');
-	RubicsCube Orange_side('O');
-	RubicsCube Red_side('R');
-	UpRotatingByClockwise(Blue_side.array_cube_color, Green_side.array_cube_color, Orange_side.array_cube_color, Red_side.array_cube_color);
-	UpRotatingByClockwise(Blue_side.array_cube_color, Green_side.array_cube_color, Orange_side.array_cube_color, Red_side.array_cube_color);
-	UpRotatingByClockwise(Blue_side.array_cube_color, Green_side.array_cube_color, Orange_side.array_cube_color, Red_side.array_cube_color);
-	Blue_side.get_Cube_side();
+	RubicsCube Front_side	('B');
+	RubicsCube Up_side		('Y');
+	RubicsCube Right_side	('R');
+	RubicsCube Back_side	('G');
+	RubicsCube Left_side	('O');
+	RubicsCube Down_side	('W');
+
+	UpRotatingByClockwise(Front_side.array_cube_color, Left_side.array_cube_color, Back_side.array_cube_color, Right_side.array_cube_color, Up_side.array_cube_color);
+	RightRotatingByAntiClockWise(Up_side.array_cube_color, Back_side.array_cube_color, Front_side.array_cube_color, Down_side.array_cube_color, Right_side.array_cube_color);
+
+	cout << "Front: " << endl;		Front_side.get_Cube_side();		cout << endl;
+	cout << "Up:    " << endl;		Up_side.   get_Cube_side();		cout << endl;
+	cout << "Left:  " << endl;		Left_side. get_Cube_side();		cout << endl;
+	cout << "Back:  " << endl;		Back_side. get_Cube_side();		cout << endl;
+	cout << "Right: " << endl;		Right_side.get_Cube_side();		cout << endl;
+	cout << "Down:  " << endl;		Down_side. get_Cube_side();		cout << endl;
 }
 
-template <typename a, typename b, typename c, typename d>
-void Game::UpRotatingByClockwise(a firstColor, b secondColor, c thirdColor, d fourthColor)
+template <typename a, typename b, typename c, typename d, typename e>
+void Game::UpRotatingByClockwise(a FrontColor, b LeftColor, c backColor, d rightcolor, e upcolor)
 {
 	system("cls");
-	RubicsCube cubes_number;
-	int number = cubes_number.get_number_of_cubes();
-	char Result;
-	int tempArr[3][3]{};
+	
 	for (int i = 0; i < 3; i++)
-		tempArr[0][i] = firstColor[0][i];
+		tempArr[0][i] = FrontColor[0][i];	//сохранил позицию Up сайда верхней грани.
 
+	//данный цикл, запускает поворот грани по часовой стрелке
 	for (int i = 0; i < number; i++)
 	{
-		firstColor[0][i] = secondColor[0][i];
-		secondColor[0][i] = thirdColor[0][i];
-		thirdColor[0][i] = fourthColor[0][i];
-		fourthColor[0][i] = tempArr[0][i];
+		FrontColor[0][i] = rightcolor[0][i];
+		rightcolor[0][i] = backColor[0][i];
+		backColor[0][i] = LeftColor[0][i];
+		LeftColor[0][i] = tempArr    [0][i];
+	}
+
+	//cохранил текущее положение Up сайда
+	for (int i = 0; i < number; i++)
+	{
+		temp1up[0][i] = upcolor[0][i]; //верхняя полоса
+		temp2up[i][2] = upcolor[i][2]; //правая полоса
+		temp3up[2][i] = upcolor[2][i]; //левая полоса
+		temp4up[i][0] = upcolor[i][0]; //нижняя полоса
+	}
+	
+	//изменение позиционирования цветов на Up сайде.
+	for (int i = 0; i < number; i++)
+	{
+		upcolor[i][2] = temp1up[0][i]; // из верхней в правую
+		upcolor[i][0] = temp2up[i][2]; // из правой в нижнюю
+		upcolor[2][i] = temp4up[i][0]; //из нижней в левую
+		upcolor[0][i] = temp3up[2][i]; //из левой в верхнюю
+	}
+}
+
+template <typename a, typename b, typename c, typename d, typename e>
+void Game::RightRotatingByAntiClockWise(a UpColor, b backColor, c FrontColor, d DownColor, e rightColor)
+{
+	system("cls");
+
+	for (int i = 0; i < 3; i++)
+		tempArr[i][2] = UpColor[i][2];	//сохранил позицию Up сайда верхней грани.
+
+											//данный цикл, запускает поворот грани по часовой стрелке
+	for (int i = 0; i < number; i++)
+	{
+		UpColor[i][2] = FrontColor[i][2];
+		FrontColor[i][2] = DownColor[i][2];
+		DownColor [i][2] = backColor [i][2];
+		backColor[i][2] = tempArr[i][2] ;
+	}
+
+	//cохранил текущее положение Up сайда
+	for (int i = 0; i < number; i++)
+	{
+		temp1right[0][i] = rightColor[0][i]; //верхняя полоса
+		temp2right[i][2] = rightColor[i][2]; //правая полоса
+		temp3right[2][i] = rightColor[2][i]; //левая полоса
+		temp4right[i][0] = rightColor[i][0]; //нижняя полоса
+	}
+
+	//изменение позиционирования цветов на Up сайде.
+	for (int i = 0; i < number; i++)
+	{
+		rightColor[i][2] = temp1right[0][i]; // из верхней в правую
+		rightColor[i][0] = temp2right[i][2]; // из правой в нижнюю
+		rightColor[2][i] = temp3right[i][0]; //из нижней в левую
+		rightColor[0][i] = temp4right[2][i]; //из левой в верхнюю
 	}
 }
 
 Game::~Game()
 {
-	cout << "Destructor, Game!" << endl;
+	//cout << "Destructor, Game!" << endl;
 }
--- src/Rubic'sCube/Rubic'sCube/Game.h
+++ src/Rubic'sCube/Rubic'sCube/Game.h
@@ -3,12 +3,21 @@
 
 class Game
 {
-public:
-	void fGameLoop();
-	template <typename a, typename b, typename c, typename d>
-	void UpRotatingByClockwise(a firstColor, b secondColor, c thirdColor, d fourthColor);
+	RubicsCube cubes_number;
+	int number = cubes_number.get_number_of_cubes();
+	char Result;
+	int tempArr[3][3]{};
+	int temp1up[3][3], temp2up[3][3], temp3up[3][3], temp4up[3][3], temp1right[3][3], temp2right[3][3], temp3right[3][3], temp4right[3][3];
+
+private:
+	
+	template <typename a, typename b, typename c, typename d, typename e>
+	void UpRotatingByClockwise(a firstColor, b secondColor, c thirdColor, d fourthColor, e fithColor);
+	template <typename a, typename b, typename c, typename d, typename e>
+	void RightRotatingByAntiClockWise(a firstColor, b secondColor, c thirdColor, d fourthColor, e fithColor);
 
 public:
 	Game();
+	void fGameLoop();
 	virtual ~Game();
 };

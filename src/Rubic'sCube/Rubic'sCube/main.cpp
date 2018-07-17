a253972c5e4a540d826902298b1ff7dcd8151e02
OLutchak <OLutchak@luxoft.com>
2018-07-17 11:15:19 +0300

    mod src

--- src/Rubic'sCube/Rubic'sCube/main.cpp
+++ src/Rubic'sCube/Rubic'sCube/main.cpp
@@ -14,12 +14,12 @@ void main()
 
 void initialize_cubes()
 {
-	Cube Blue_side('B'); cout << endl;
+	Cube Blue_side	('B'); cout << endl;
 	Cube Yellow_side('Y'); cout << endl;
-	Cube Red_side('R'); cout << endl;
+	Cube Red_side	('R'); cout << endl;
 	Cube Orange_side('O'); cout << endl;
-	Cube Green_side('G'); cout << endl;
-	Cube White_side('W'); cout << endl;
+	Cube Green_side	('G'); cout << endl;
+	Cube White_side	('W'); cout << endl;
 
 	system("pause");
 }
